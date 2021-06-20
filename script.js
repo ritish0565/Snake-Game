69f480765a597ae9cbf9d7492390161de4c11519
Ritish <ritish0565.cse19@chitkara.edu.in>
2021-06-20 15:06:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,172 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext('2d');
+
+class SnakePart{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const snakeParts = [];
+let tailLength = 2;
+
+let speed = 7;
+
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+let appleX = 5;
+let appleY = 5;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
+let score = 0;
+
+//Game loop
+function drawGame(){
+
+  changeSnakePosition();
+  let result = isGameOver();
+  if(result) return;
+
+  clearScreen();
+  
+
+  checkAppleCollision();
+  drawApple();
+  drawSnake();
+
+  drawScore();
+
+  // if(score > 2){
+  //   speed = 11;
+  // }
+  // if(score > 5){
+  //   speed = 15;
+  // }
+
+  setTimeout(drawGame, 1000/speed);
+}
+
+function isGameOver(){
+  let gameOver = false;
+
+  if(yVelocity === 0 && xVelocity === 0){
+    return false;
+  }
+  //walls
+  if(headX < 0) gameOver = true;
+  else if(headX >= tileCount) gameOver = true;
+  else if(headY >= tileCount) gameOver = true;
+  else if(headY < 0) gameOver = true;
+
+  //own body
+  for(let i=0;i<snakeParts.length;i++){
+    let part = snakeParts[i];
+    if(part.x === headX && part.y === headY){
+      gameOver = true;
+      break;
+    }
+  }
+
+  if(gameOver){
+    ctx.fillStyle = 'white';
+    ctx.font = "50px Verdana";
+
+    var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop("0", " magenta");
+    gradient.addColorStop("0.5", "blue");
+    gradient.addColorStop("1.0", "red");
+    // Fill with gradient
+    ctx.fillStyle = gradient;
+
+    ctx.fillText("Gamer Over!", canvas.width / 6.5, canvas.height / 2);
+  }
+
+  return gameOver;
+}
+
+function drawScore(){
+  ctx.fillStyle = "white";
+  ctx.font = "10px Verdana";
+  ctx.fillText("Score : " + score, canvas.width - 50, 10);
+}
+
+
+function checkAppleCollision(){
+  if(appleX === headX && appleY === headY){
+      appleX = Math.floor(Math.random()*tileCount);
+      appleY = Math.floor(Math.random()*tileCount);
+      tailLength++;
+      score++;
+      speed++;
+  }
+}
+
+function drawApple(){
+  ctx.fillStyle = 'red';
+  ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition(){
+  headX = headX + xVelocity;
+  headY = headY + yVelocity;
+
+}
+
+function clearScreen(){
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake(){
+  
+
+  ctx.fillStyle = 'green';
+  for(let i=0;i<snakeParts.length;i++){
+      let part = snakeParts[i];
+      ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  } 
+   
+  snakeParts.push(new SnakePart(headX, headY)); //put an item at the end of the list next to the head
+
+  if(snakeParts.length > tailLength){
+    snakeParts.shift();  //remove the farthest ite from the snake part if we have more than the tail size
+
+  }
+
+  ctx.fillStyle = "orange";
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event){
+  if(event.keyCode == 38){
+    if(yVelocity == 1)
+      return;
+    yVelocity = -1;
+    xVelocity = 0;
+  }else if(event.keyCode == 40){
+    if(yVelocity == -1)
+      return;
+    yVelocity = 1;
+    xVelocity = 0;
+  }else if(event.keyCode == 37){
+    if(xVelocity == 1)
+      return;
+    yVelocity = 0;
+    xVelocity = -1;
+  }else if(event.keyCode == 39){
+    if(xVelocity == -1)
+      return;
+    yVelocity = 0;
+    xVelocity = 1;
+  }
+}
+
+drawGame();
